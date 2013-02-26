2a86cccacddf6a34a590622364fa2a197120b380
Iraklis Georgas <iraklis.g@gmail.com>
2013-02-26 15:44:35 +0200

    hey

--- edx_600x/L4/l4_p9_odd.py
+++
@@ -1,9 +0,0 @@
-def odd(x):
-    '''
-    x: int or float.
-
-    returns: True if x is odd, False otherwise
-    '''
-    return bool(x%2)
-
-print odd(3)
--- only_to_test/test_folder/test_module1.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on Feb 22, 2013
-
-@author: ira
-'''
