2ff8a41afce7dd0459d5d1ddbcdbf9dae7ac77e5
Iraklis Georgas <iraklis.g@gmail.com>
2013-03-01 11:41:45 +0200

    dsd

--- edx600x/L5 - Recursion/L5_P8_bisection_search_on_strings_recursion.py
+++ edx600x/L5 - Recursion/L5_P8_bisection_search_on_strings_recursion.py
@@ -1,5 +1,8 @@
-'''
-Created on Feb 27, 2013
-
-@author: ira
-'''
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
+    
+    returns: True if char is in aStr; False otherwise
+    '''
+    # Your code here
