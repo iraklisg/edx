98a2d52f1ab056a81496e050339a1bcd9cbbe99b
ira <iraklis.g@gmail.com>
2013-03-02 19:55:00 +0200

    end of hangman part 1/3

---
+++ edx600x/Problem_Set_3/ps3_newton.py
@@ -0,0 +1,55 @@
+# 6.00x Problem Set 3
+#
+# Successive Approximation: Newton's Method
+#
+
+
+# Problem 1: Polynomials
+def evaluatePoly(poly, x):
+    '''
+    Computes the value of a polynomial function at given value x. Returns that
+    value as a float.
+ 
+    poly: list of numbers, length > 0
+    x: number
+    returns: float
+    '''
+    # FILL IN YOUR CODE HERE...
+
+
+
+
+
+
+
+
+# Problem 2: Derivatives
+def computeDeriv(poly):
+    '''
+    Computes and returns the derivative of a polynomial function as a list of
+    floats. If the derivative is 0, returns [0.0].
+ 
+    poly: list of numbers, length &gt; 0
+    returns: list of numbers (floats)
+    '''
+    # FILL IN YOUR CODE HERE...
+
+
+
+
+
+# Problem 3: Newton's Method
+def computeRoot(poly, x_0, epsilon):
+    '''
+    Uses Newton's method to find and return a root of a polynomial function.
+    Returns a list containing the root and the number of iterations required
+    to get to the root.
+ 
+    poly: list of numbers, length > 1.
+         Represents a polynomial function containing at least one real root.
+         The derivative of this polynomial function at x_0 is not 0.
+    x_0: float
+    epsilon: float > 0
+    returns: list [float, int]
+    '''
+    # FILL IN YOUR CODE HERE...
