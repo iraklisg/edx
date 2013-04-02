6932c488f126d53bd6a62ac3c345b2cdc3df4dc6
ira <iraklis.g@gmail.com>
2013-04-03 01:12:22 +0300

    fy

--- edx600x/L12_Plotting/L12_P3_read_from_file.py
+++ edx600x/L12_Plotting/L12_P3_read_from_file.py
@@ -3,6 +3,16 @@ import pylab
 
 inFile = open('julyTemps.txt')
 line = inFile.readline()
-print line
-fields = inFile.readlines()
+line = inFile.readline()
+fields = line.split(" ")
+if len(fields) < 3 or not fields[0].isdigit():
+    print "malakia"
+line = inFile.readline()
+line = inFile.readline()
+line = inFile.readline()
+line = inFile.readline()
+line = inFile.readline()
+fields = line.split(" ")
 print fields
+if len(fields) < 3 or not fields[0].isdigit():
+    print "malakia"
