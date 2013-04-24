5192a6dcee281345bf557eafebd0cca919a49641
ira <iraklis.g@gmail.com>
2013-04-24 18:08:05 +0300

    hey

---
+++ edx600x/L16_Using_Randomness_to_solve_non-random_problems/L16_1_Normal_distributions.py
@@ -0,0 +1,27 @@
+import random, pylab
+
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles 
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+
+
+
+def makeNormal(mean, sd, numSamples):
+    samples = []
+    for i in range(numSamples):
+        samples.append(random.gauss(mean, sd))
+    pylab.hist(samples, bins = 101)
+
+makeNormal(0, 1.0, 100000)
+pylab.show()
+
+
+
+
