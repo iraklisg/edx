bc8e0d78d40c33d4ad276402714d82fbbd0eb395
ira <iraklis.g@gmail.com>
2013-04-25 23:14:59 +0300

    po

---
+++ edx600x/L17_Curve_Fitting/L16_3_Curve_fitting_Find_a_model.py
@@ -0,0 +1,5 @@
+'''
+Created on Apr 25, 2013
+
+@author: ira
+'''
--- edx600x/L17_Curve_Fitting/L17_2_Errors_in_experimental_observations.py
+++ edx600x/L17_Curve_Fitting/L17_2_Errors_in_experimental_observations.py
@@ -14,13 +14,30 @@ def getData(fileName):
 
 def plotData(fileName):
     xVals, yVals = getData(fileName)
-    xVals = pylab.array(xVals)
+    xVals = pylab.array(xVals) # pylab arrays
     yVals = pylab.array(yVals)
     xVals = xVals*9.81 # convert mass to force (F = mg)
+    #Do the plotting
     pylab.plot(xVals, yVals, 'bo', label = 'Measured displacements')
     pylab.title('Measured Displacement of Spring')
     pylab.xlabel('Force (Newtons)')
     pylab.ylabel('Distance (meters)')
 
-plotData('springData.txt')
+#plotData('springData.txt')
+#pylab.show()
+
+def testErrors(ntrials=10000,npts=100):
+    results = [0] * ntrials
+    for i in xrange(ntrials):
+        s = 0   # sum of random points
+        for j in xrange(npts):
+            s += random.triangular(-1,1)
+        results[i] =s
+    # plot results in a histogram
+    pylab.hist(results,bins=50)
+    pylab.title('Sum of 100 random points -- Triangular PDF (10,000 trials)')
+    pylab.xlabel('Sum')
+    pylab.ylabel('Number of trials')
+
+testErrors()
 pylab.show()
