417dbbdd1746000cdcfb37b0b7111922294d62ee
ira <iraklis.g@gmail.com>
2013-04-27 01:51:38 +0300

    sd

--- edx600x/Problem_Set_9/helper.py
+++ edx600x/Problem_Set_9/helper.py
@@ -1,21 +1,31 @@
 import random, pylab
 
-x = [random.random() for i in xrange(1000)]
-y = [2*random.random() for i in xrange(1000)]
+x = [random.random() for i in xrange(100)]
+y = [2*random.random() for i in xrange(100)]
 
 # subplot sharing the same axis (http://matplotlib.org/examples/pylab_examples/subplots_demo.html)
 f, axarr = pylab.subplots(2, sharex=True)
 
 print f
 print axarr
-
+# create an empty figure 1
+pylab.figure(1)
+# inside that figure 1 draw 2 subplots, one plot and one hist
 axarr[0].plot(x, y , 'bo')
 axarr[1].hist(y, bins=100)
-pylab.show()
 
 #subplot not share the same axis (http://matplotlib.org/examples/pylab_examples/subplot_demo.html)
+# create a second empty figure 2
+pylab.figure(2)
+# inside that figure 1 draw 2 subplots, one plot 211 and one hist 212
 pylab.subplot(211)
-pylab.plot(x, y)
+pylab.plot(x, y, 'bo')
 pylab.subplot(212)
 pylab.hist(y, bins=100)
+# finally show all previously created figures 1 and 2
+#pylab.show()
+
+# how many bins for hist?
+pylab.figure("how many bins?")
+pylab.hist(x, bins=10)
 pylab.show()
--- edx600x/Problem_Set_9/ps9.py
+++ edx600x/Problem_Set_9/ps9.py
@@ -3,59 +3,21 @@
 import numpy
 import random
 import pylab
-from ps8b import *
+#from ps8b import *
+from ps8b_precompiled_27 import *
 
 #
 # PROBLEM 1
-#        
-def simulationDelayedTreatment(numTrials):
-    """
-    Runs simulations and make histograms for problem 1.
+# helper function to plot a hist
+def plot_a_hist(populations, num_bins, num_steps):
+    '''numsteps a tuple (pre_steps, post_steps)
+    '''
+    pylab.hist(populations, bins=num_bins)
+    pylab.xlabel("Value of total viruses' population")
+    pylab.ylabel("number of occurrences for "+str(len(populations))+" num of trials")
+    pylab.title('Changes to the virus population for '+str(num_steps)+' time steps')
 
-    Runs numTrials simulations to show the relationship between delayed
-    treatment and patient outcome using a histogram.
-
-    Histograms of final total virus populations are displayed for delays of 300,
-    150, 75, 0 timesteps (followed by an additional 150 timesteps of
-    simulation).
-
-    numTrials: number of simulation runs to execute (an integer)
-    """
-    
-    # TODO
-    pre_treatment_steps = 300
-    post_treatment_steps = 150
-    
-    timesteps = [i for i in xrange(pre_treatment_steps + post_treatment_steps)] # this will be my x-axis
-    
-    accu_total_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
-#    accu_resist_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
-
-    for trial in xrange(numTrials):
-        viruses = [ResistantVirus(maxBirthProb=0.1, clearProb=0.05, resistances={'guttagonol': False}, mutProb=0.005) for i in xrange(numViruses)] # initialize a list of 100 viruses
-        patient = TreatedPatient(numViruses=100, maxPop=1000) #initialize a patient
-        for timestep in timesteps:
-            if timestep == 150:
-
-                patient.addPrescription('guttagonol')
-
-            accu_total_virus_populations[timestep] += patient.update()
-
-#            accu_resist_virus_populations[timestep] += patient.getResistPop(['guttagonol'])
-            
-    average_total_viruses_population = [p/float(numTrials) for p in accu_total_virus_populations]
-    average_resist_virus_populations = [p/float(numTrials) for p in accu_resist_virus_populations]
-    # PLOTTING
-    pylab.figure(1)
-    pylab.plot(timesteps, average_total_viruses_population, 'ro', label='total virus population')
-    pylab.plot(timesteps, average_resist_virus_populations, 'bo', label='resistant virus population')
-    pylab.xlabel('Number of time steps')
-    pylab.ylabel('Average size of the virus particles population')
-    pylab.title('Changes to the virus population for 300 time steps')
-    pylab.legend(loc='best')
-    pylab.show()
-
-
+# helper function to calculate the total viruses' population
 def total_viruses_population(pre_steps, post_steps=150):
     '''
     gives the total viruses population values for given steps
@@ -76,8 +38,8 @@ def total_viruses_population(pre_steps, post_steps=150):
     
     # Initialize the total number of steps
     timesteps = [i for i in xrange(pre_steps + post_steps)] # this will be my x-axis
-    # Initialize the total viruses' population for each timestep to be equal to 0
-    accu_total_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
+    # Initialize the total viruses' population as an empty list
+    accu_total_virus_populations = [0.0 for i in timesteps]
     # Initialize a list of numViruses particles of ResistantVirus
     viruses = [ResistantVirus(maxBirthProb, clearProb, resistances, mutProb) for i in xrange(numViruses)]
     # Initialize a Treated Patient with numViruses particles of ResistantVirus
@@ -88,8 +50,54 @@ def total_viruses_population(pre_steps, post_steps=150):
         if timestep == pre_steps: # After pre_steps a give patient a drug
             patient.addPrescription('guttagonol')
         # I calculate the new total population of viruses' particles
-        accu_total_virus_populations[timestep] += patient.update()
-    return sum(accu_total_virus_populations)
+        accu_total_virus_populations.append(patient.update())
+    return accu_total_virus_populations[-1]
+
+def simulationDelayedTreatment(numTrials):
+    """
+    Runs simulations and make histograms for problem 1.
+
+    Runs numTrials simulations to show the relationship between delayed
+    treatment and patient outcome using a histogram.
+
+    Histograms of final total virus populations are displayed for delays of 300,
+    150, 75, 0 timesteps (followed by an additional 150 timesteps of
+    simulation).
+
+    numTrials: number of simulation runs to execute (an integer)
+    
+    """
+    viruses_population_1 = []
+    viruses_population_2 = []
+    for trial in xrange(numTrials):
+        viruses_population_1.append(total_viruses_population(pre_steps=300, post_steps=150))
+        viruses_population_2.append(total_viruses_population(pre_steps=150, post_steps=150))
+        
+
+    # PLOTTING
+    num_bins = int(max(viruses_population_1)/50)
+    pylab.figure(1)
+    #
+    pylab.subplot(221)
+    plot_a_hist(viruses_population_1, num_bins, (300, 150))
+    #
+    pylab.subplot(222)
+    plot_a_hist(viruses_population_2, num_bins, (150, 150))
+    #
+    pylab.subplot(223)
+    plot_a_hist(viruses_population_2, num_bins, (75, 150))
+    #
+    pylab.subplot(224)
+    plot_a_hist(viruses_population_2, num_bins, (0, 150))
+    #
+
+#    pylab.legend(loc='best')
+    pylab.show()
+
+
+simulationDelayedTreatment(200)
+
+    
 #
 # PROBLEM 2
 #
