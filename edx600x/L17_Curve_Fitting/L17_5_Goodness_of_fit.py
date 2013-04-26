a11ef8fcdd6ebd0ea74ee845294c6b4405b4eb37
ira <ira@wicear_lab>
2013-04-26 17:50:28 +0300

    end of L17.5

---
+++ edx600x/L17_Curve_Fitting/L17_5_Goodness_of_fit.py
@@ -0,0 +1,77 @@
+import random, pylab
+
+def getTrajectoryData(fileName):
+    dataFile = open(fileName, 'r')
+    distances = []
+    heights1, heights2, heights3, heights4 = [],[],[],[]
+    discardHeader = dataFile.readline()
+    for line in dataFile:
+        d, h1, h2, h3, h4 = line.split()
+        distances.append(float(d))
+        heights1.append(float(h1))
+        heights2.append(float(h2))
+        heights3.append(float(h3))
+        heights4.append(float(h4))
+    dataFile.close()
+    return (distances, [heights1, heights2, heights3, heights4])
+
+def tryFits(fName):
+    distances, heights = getTrajectoryData(fName)
+    distances = pylab.array(distances)*36 # convert yards to feet
+    # calculate the average height based on values from all the 4 experiments
+    totHeights = pylab.array([0]*len(distances))
+    for h in heights:
+        totHeights = totHeights + pylab.array(h)
+    # begin to plot
+    pylab.title('Trajectory of Projectile (Mean of 4 Trials)')
+    pylab.xlabel('Inches from Launch Point')
+    pylab.ylabel('Inches Above Launch Point')
+    meanHeights = totHeights/float(len(heights))
+    pylab.plot(distances, meanHeights, 'bo')
+    a,b = pylab.polyfit(distances, meanHeights, 1)
+    altitudes = a*distances + b
+    pylab.plot(distances, altitudes, 'r',
+               label = 'Linear Fit')
+    a,b,c = pylab.polyfit(distances, meanHeights, 2)
+    altitudes = a*(distances**2) + b*distances + c
+    pylab.plot(distances, altitudes, 'g',
+               label = 'Quadratic Fit')
+    pylab.legend()
+
+##tryFits('launcherData.txt')
+##pylab.show()
+
+def rSquare(measured, estimated):
+    """measured: one dimensional array of measured values
+       estimate: one dimensional array of predicted values"""
+    SEE = ((estimated - measured)**2).sum() # calculate the variability of errors (predictions - observations)
+    mMean = measured.sum()/float(len(measured))
+    MV = ((mMean - measured)**2).sum() # calculate the variability of measured data (observations)
+    return 1 - SEE/MV
+
+def tryFits1(fName):
+    distances, heights = getTrajectoryData(fName)
+    distances = pylab.array(distances)*36
+    totHeights = pylab.array([0]*len(distances))
+    for h in heights:
+        totHeights = totHeights + pylab.array(h)
+    pylab.title('Trajectory of Projectile (Mean of 4 Trials)')
+    pylab.xlabel('Inches from Launch Point')
+    pylab.ylabel('Inches Above Launch Point')
+    meanHeights = totHeights/float(len(heights))
+    pylab.plot(distances, meanHeights, 'bo')
+    a,b = pylab.polyfit(distances, meanHeights, 1)
+    altitudes = a*distances + b
+    pylab.plot(distances, altitudes, 'r',
+               label = 'Linear Fit' + ', R2 = '
+               + str(round(rSquare(meanHeights, altitudes), 4)))
+    a,b,c = pylab.polyfit(distances, meanHeights, 2)
+    altitudes = a*(distances**2) + b*distances + c
+    pylab.plot(distances, altitudes, 'g',
+               label = 'Quadratic Fit' + ', R2 = '
+               + str(round(rSquare(meanHeights, altitudes), 4)))
+    pylab.legend()
+
+tryFits1('launcherData.txt')
+pylab.show()
+
