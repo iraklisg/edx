8828ce33f8ef65c12189c7663736359172d55f8b
ira <iraklis.g@gmail.com>
2013-04-26 20:44:34 +0300

    subplots

--- edx600x/Problem_Set_9/helper.py
+++ edx600x/Problem_Set_9/helper.py
@@ -1,5 +1,21 @@
-'''
-Created on Apr 26, 2013
+import random, pylab
 
-@author: ira
-'''
+x = [random.random() for i in xrange(1000)]
+y = [2*random.random() for i in xrange(1000)]
+
+# subplot sharing the same axis (http://matplotlib.org/examples/pylab_examples/subplots_demo.html)
+f, axarr = pylab.subplots(2, sharex=True)
+
+print f
+print axarr
+
+axarr[0].plot(x, y , 'bo')
+axarr[1].hist(y, bins=100)
+pylab.show()
+
+#subplot not share the same axis (http://matplotlib.org/examples/pylab_examples/subplot_demo.html)
+pylab.subplot(211)
+pylab.plot(x, y)
+pylab.subplot(212)
+pylab.hist(y, bins=100)
+pylab.show()
--- edx600x/Problem_Set_9/ps9.py
+++ edx600x/Problem_Set_9/ps9.py
@@ -23,12 +23,73 @@ def simulationDelayedTreatment(numTrials):
     """
     
     # TODO
+    pre_treatment_steps = 300
+    post_treatment_steps = 150
+    
+    timesteps = [i for i in xrange(pre_treatment_steps + post_treatment_steps)] # this will be my x-axis
+    
+    accu_total_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
+#    accu_resist_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
+
+    for trial in xrange(numTrials):
+        viruses = [ResistantVirus(maxBirthProb=0.1, clearProb=0.05, resistances={'guttagonol': False}, mutProb=0.005) for i in xrange(numViruses)] # initialize a list of 100 viruses
+        patient = TreatedPatient(numViruses=100, maxPop=1000) #initialize a patient
+        for timestep in timesteps:
+            if timestep == 150:
+
+                patient.addPrescription('guttagonol')
+
+            accu_total_virus_populations[timestep] += patient.update()
+
+#            accu_resist_virus_populations[timestep] += patient.getResistPop(['guttagonol'])
+            
+    average_total_viruses_population = [p/float(numTrials) for p in accu_total_virus_populations]
+    average_resist_virus_populations = [p/float(numTrials) for p in accu_resist_virus_populations]
+    # PLOTTING
+    pylab.figure(1)
+    pylab.plot(timesteps, average_total_viruses_population, 'ro', label='total virus population')
+    pylab.plot(timesteps, average_resist_virus_populations, 'bo', label='resistant virus population')
+    pylab.xlabel('Number of time steps')
+    pylab.ylabel('Average size of the virus particles population')
+    pylab.title('Changes to the virus population for 300 time steps')
+    pylab.legend(loc='best')
+    pylab.show()
 
 
-
-
-
-
+def total_viruses_population(pre_steps, post_steps=150):
+    '''
+    gives the total viruses population values for given steps
+    pre_steps: <int> the number of steps before treatment
+    post_steps: <int> the number of steps after treatment
+    
+    returns <int> the total viruses' population
+    
+    '''
+    # Initialize the Resistance virus parameters
+    maxBirthProb=0.1
+    clearProb=0.05
+    resistances={'guttagonol': False}
+    mutProb=0.005
+    # Initialize the Resistant patient parameters
+    numViruses=100
+    maxPop=1000
+    
+    # Initialize the total number of steps
+    timesteps = [i for i in xrange(pre_steps + post_steps)] # this will be my x-axis
+    # Initialize the total viruses' population for each timestep to be equal to 0
+    accu_total_virus_populations = [0.0 for i in timesteps] # initialize an array with #timesteps=300 zeros
+    # Initialize a list of numViruses particles of ResistantVirus
+    viruses = [ResistantVirus(maxBirthProb, clearProb, resistances, mutProb) for i in xrange(numViruses)]
+    # Initialize a Treated Patient with numViruses particles of ResistantVirus
+    patient = TreatedPatient(viruses, maxPop)
+    
+    # For every step
+    for timestep in timesteps:
+        if timestep == pre_steps: # After pre_steps a give patient a drug
+            patient.addPrescription('guttagonol')
+        # I calculate the new total population of viruses' particles
+        accu_total_virus_populations[timestep] += patient.update()
+    return sum(accu_total_virus_populations)
 #
 # PROBLEM 2
 #
