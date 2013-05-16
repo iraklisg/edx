c4541500838d76292f20e5f67c146c84c2880dc3
ira <ira@gyzi>
2013-05-16 19:28:19 +0300

    hey

--- edx600x/L19_Graphs/L19_P2_Students_permutations.py
+++ edx600x/L19_Graphs/L19_P2_Students_permutations.py
@@ -61,13 +61,28 @@ class Graph(Digraph):
         rev = Edge(edge.getDestination(), edge.getSource()) #create a new instance of Edge with reversed src and dest
         Digraph.addEdge(self, rev)
 
+nodes = []
+# Create all nodes and add them to the list above
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+# add nodes to the graph
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+    
+# create the appropriate edges under the condition
+# edges connect two permutations if one can be made into the other by swapping two adjacent students
+# then add the edges to the graph
 
-def printPath(path):
-    # a path is a list of nodes
-    result = ''
-    for i in range(len(path)):
-        if i == len(path) - 1:
-            result = result + str(path[i])
-        else:
-            result = result + str(path[i]) + '->'
-    return result
+# Graph.addEge(edge)
+# edge = Edge(src, dest)
+g.addEdge(Edge(nodes[0],nodes[1]))
+g.addEdge(Edge(nodes[0],nodes[2]))
+g.addEdge(Edge(nodes[1],nodes[4]))
+g.addEdge(Edge(nodes[2],nodes[3]))
+g.addEdge(Edge(nodes[3],nodes[5]))
+g.addEdge(Edge(nodes[4],nodes[5]))
