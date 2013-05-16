27975a04e703a23c9131a5bee021375564696c5b
ira <ira@gyzi>
2013-05-16 19:33:06 +0300

    y

--- edx600x/L18_Optimization_Problems/L18_4_Find_the_optimum_solution.py
+++ edx600x/L18_Optimization_Problems/L18_4_Find_the_optimum_solution.py
@@ -1,27 +1,6 @@
 from itertools import *
+from L18_3_The_Greedy_Algorithm import *
 
-class Item(object):
-    def __init__(self, n, v, w):
-        self.name = n
-        self.value = float(v)
-        self.weight = float(w)
-    def getName(self):
-        return self.name
-    def getValue(self):
-        return self.value
-    def getWeight(self):
-        return self.weight
-    def __str__(self):
-        return '<' + self.name + ', ' + str(self.value) + ', '\
-                     + str(self.weight) + '>'
-
-def buildItems():
-    return [Item(n,v,w) for n,v,w in (('clock', 175, 10),
-                                      ('painting', 90, 9),
-                                      ('radio', 20, 4),
-                                      ('vase', 50, 2),
-                                      ('book', 10, 1),
-                                      ('computer', 200, 20))]
 
 def allCombos(items):
     # Your code here
