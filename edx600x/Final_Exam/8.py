2601dd64325405f726315cf77b07ddeb5727f333
ira <ira@gyzi>
2013-06-03 20:00:37 +0300

    8

--- edx600x/Final_Exam/8.py
+++ edx600x/Final_Exam/8.py
@@ -19,50 +19,50 @@ class Frob(object):
         return self.myName()
 
 
-# frobs = [Frob('eric')]
-# def insert(atMe, newFrob):
-#     """
-#     atMe: a Frob that is part of a doubly linked list
-#     newFrob:a Frob with no links
-#     This procedure appropriately inserts newFrob into the linked list that atMe is a part of.    
-#     """
-#     # Your Code Here
-#     # assume that the atMe is already part of the linked list
-#     # Append the newFrob in list and then sort the list by name
-#     frobs.append(newFrob)
-#     sorted_frobs = sorted(frobs, key = metric, reverse = False)
-#     print sorted_frobs
-#     # find the index of the newFrob
-#     i = sorted_frobs.index(newFrob)
-#     print 'index = ',i
-#     # Go to the previous frob and set after=newFrob
-#     if i-1 >= 0: sorted_frobs[i-1].setAfter(newFrob)
-# #     else: sorted_frobs[i-1].setAfter(None)
-#     # Go to the next frob and set before=newFrob
-#     if i+1 <= len(sorted_frobs)-1: sorted_frobs[i+1].setBefore(newFrob)
-# #     else: sorted_frobs[i+1].setBefore(None)
-# 
-#      
-# # metric: use item's value
-# def metric(item):
-#     return item.myName()
-
-
+frobs = [Frob('eric')]
 def insert(atMe, newFrob):
     """
     atMe: a Frob that is part of a doubly linked list
+    newFrob:a Frob with no links
     This procedure appropriately inserts newFrob into the linked list that atMe is a part of.    
     """
     # Your Code Here
     # assume that the atMe is already part of the linked list
-    atMe_name = atMe.myName()
-    newFrob_name = newFrob.myName()
-    if atMe_name >= newFrob_name: #if atMe = fred and newFrob = eric
-        newFrob.setAfter(atMe)# after eric is fred 
-        atMe.setBefore(newFrob)# befor fred is eric
-    else:
-        newFrob.setBefore(atMe)
-        atMe.setAfter(newFrob)
+    # Append the newFrob in list and then sort the list by name
+    frobs.append(newFrob)
+    sorted_frobs = sorted(frobs, key = metric, reverse = False)
+    print sorted_frobs
+    # find the index of the newFrob
+    i = sorted_frobs.index(newFrob)
+    print 'index = ',i
+    # Go to the previous frob and set after=newFrob
+    if i-1 >= 0: sorted_frobs[i-1].setAfter(newFrob)
+#     else: sorted_frobs[i-1].setAfter(None)
+    # Go to the next frob and set before=newFrob
+    if i+1 <= len(sorted_frobs)-1: sorted_frobs[i+1].setBefore(newFrob)
+#     else: sorted_frobs[i+1].setBefore(None)
+
+     
+# metric: use item's value
+def metric(item):
+    return item.myName()
+
+
+# def insert(atMe, newFrob):
+#     """
+#     atMe: a Frob that is part of a doubly linked list
+#     This procedure appropriately inserts newFrob into the linked list that atMe is a part of.    
+#     """
+#     # Your Code Here
+#     # assume that the atMe is already part of the linked list
+#     atMe_name = atMe.myName()
+#     newFrob_name = newFrob.myName()
+#     if atMe_name >= newFrob_name: #if atMe = fred and newFrob = eric
+#         newFrob.setAfter(atMe)# after eric is fred 
+#         atMe.setBefore(newFrob)# befor fred is eric
+#     else:
+#         newFrob.setBefore(atMe)
+#         atMe.setAfter(newFrob)
 
 eric = Frob('eric')
 andrew = Frob('andrew')
@@ -76,14 +76,14 @@ insert(eric, fred)
 insert(ruth, martha)
 
 guys = [fred, andrew, martha, eric, ruth]
-# sorted_guys = sorted(guys, key = metric, reverse = False)
+sorted_guys = sorted(guys, key = metric, reverse = False)
 
-for guy in guys:
-    print '{} --> ({}) --> {}'.format(guy.getBefore(), guy.myName(), guy.getAfter())
+# for guy in guys:
+#     print '{} --> ({}) --> {}'.format(guy.getBefore(), guy.myName(), guy.getAfter())
 
 
 
 # print sorted_guys
-# 
-# for guy in sorted_guys:
-#     print '{} --> ({}) --> {}'.format(guy.getBefore(), guy.myName(), guy.getAfter())
+
+for guy in sorted_guys:
+    print '{} --> ({}) --> {}'.format(guy.getBefore(), guy.myName(), guy.getAfter())
